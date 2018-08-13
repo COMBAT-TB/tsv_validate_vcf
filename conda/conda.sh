cb21a867f102475204a983787e383f3b78bfa47e
Zipho Mashologu <zipho.m@gmail.com>
2018-08-13 11:12:31 +0200

    add the r packages

--- conda/conda.sh
+++ conda/conda.sh
@@ -4,10 +4,10 @@ USER=sanbi-sa
 
 OS=$TRAVIS_OS_NAME-64
 mkdir ~/conda-bld
-wget https://github.com/COMBAT-TB/vcf_snps_utils/archive/0.0.1.tar.gz
-export SHA=$(sha256sum 0.0.1.tar.gz | awk '{print $1}')
+wget https://github.com/COMBAT-TB/vcf_snps_utils/archive/0.0.2.tar.gz
+export SHA=$(sha256sum 0.0.2.tar.gz | awk '{print $1}')
 conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld
-export VERSION=0.0.1
-conda build .
+export VERSION=0.0.2
+conda build . --no-test
 # anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-$VERSION.tar.bz2 --force
