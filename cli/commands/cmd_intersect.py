41cc8b009440fa501f49844bbdac12a5de3bc65a
Zipho Mashologu <zipho.m@gmail.com>
2018-08-22 12:17:23 +0200

    changes to the files

--- cli/commands/cmd_intersect.py
+++ cli/commands/cmd_intersect.py
@@ -7,17 +7,24 @@ import time
 import vcf
 from pathlib import Path
 
-from cli.cli import pass_context
+try: 
+    from cli.cli import pass_context    
+    from cli.commands.util import pbopen
+    from cli.commands.report import generate_report
+except (ImportError, ValueError):
+    from cli.cli import pass_context
+    from cli.commands.util import pbopen
+    from cli.commands.report import generate_report
+
 from pprint import pprint as pp
-from cli.commands.util import pbopen
 
 
 @click.command('intersect', short_help='Intersect SNPs with a CSV file.')
 @click.argument('vcffiles', nargs=-1, type=click.Path(exists=True))
 @click.argument('csvfile', type=click.Path(exists=True))
-@click.option('--out', default="/tmp/vcf_snps_utils_output", help='set the output directory, default is "/tmp/vcf_snps_utils_output/"')
+@click.option('--outdir', default="/tmp/vcf_snps_utils_output", help='set the output directory, default is "/tmp/vcf_snps_utils_output/"')
 @pass_context
-def cli(ctx, out, vcffiles, csvfile):
+def cli(ctx, outdir, vcffiles, csvfile):
     """VCF snp intersect tool (CSV file).
 
        This tools compares CSV snps to the VCF snps
@@ -44,28 +51,41 @@ def cli(ctx, out, vcffiles, csvfile):
 
     # Get the Reference - Rows (list of dicts)
     ref_rows = get_ref_rows(csvfile)
-    vcf_rows = get_vcf_rows(vcffiles)
+    ref_samples = set(ref_rows[0].keys()).difference(set(['gene','short_name','snp_type','H37Rv','H37Rv_POS']))
+    
+    # Do it for multiple VCF files
+    for vcffile in vcffiles:
+        vcf_rows = get_vcf_rows(vcffile)
 
-    # Group by position - intersects
-    intersect_result = [
-        dict(vcf=[fil for fil in filter(lambda sect_dict: sect_dict['pos'] == int(ref_dict['H37Rv_POS']), vcf_rows)],
-             csv=ref_dict) for ref_dict in ref_rows]
+        # Group by positions and get intersects
+        intersect_result = [
+            dict(vcf=[fil for fil in filter(lambda sect_dict: sect_dict['pos'] == int(ref_dict['H37Rv_POS']), vcf_rows)],
+                csv=ref_dict) for ref_dict in ref_rows]
 
-    # Refine results
-    results = f_results(intersect_result, [k for k, v in ref_rows[0].items() if
-                                           k not in ['H37Rv', 'H37Rv_POS', 'gene', 'snp_type', 'short_name', 'gene']])
+        # Refine results
+        results = f_results(intersect_result, [k for k, v in ref_rows[0].items() if
+                                            k not in ['H37Rv', 'H37Rv_POS', 'gene', 'snp_type', 'short_name', 'gene']])
 
-    # Create output files
-    create_output_files(results)
+        # Create output files
+        if not os.path.exists(outdir):
+            os.makedirs(outdir +'/vcf')         
+        output_txt_files = create_output_files(results, outdir, ref_samples)
 
-    # Trigger the VISUALISATION / PLOTTING output
-    ctx.log("###################### About to Generate Output / Visualisation")
-    output = out + "/" + time.strftime("%Y%m%d-%H%M%S")
-    call('intervene upset -i output/*.txt --type list --output {}'.format(output), shell=True, stdout=PIPE)
-    ctx.log("###################### End!")
+    #For each sample create an output
+    for ref_sample in ref_samples:
+        sample_output_dir = outdir + '/' + ref_sample
+        if not os.path.exists(sample_output_dir):
+            os.makedirs(sample_output_dir)
+        
+        # Trigger the VISUALISATION / PLOTTING output
+        ctx.log("## Create Visualisation")
+        call('intervene upset -i {0}/{1}.txt {0}/vcf/*.txt --type list --output {2}'.format(outdir, ref_sample, sample_output_dir), shell=True, stdout=PIPE)
+        ctx.log("## End")
 
-    # Generate Pandas for Stats on the results
+    # @TODO: Generate Pandas for Stats on the results
 
+    # Generate the report
+    report = generate_report(file_name='vcf_intesect_report', data=ref_samples)
 
 
 def get_ref_rows(csvfile):
@@ -75,16 +95,13 @@ def get_ref_rows(csvfile):
     return rows
 
 
-def get_vcf_rows(vcffiles):
-    pp(vcffiles)
-    for vcffile in vcffiles:
-        with pbopen(click.format_filename(vcffile)) as f:
-            vcf_rows = vcf.Reader(f)
-            pp(vcffile)
-            vcf_dict_list = [
-                dict(pos=int(vcf_row.POS), ref=vcf_row.REF, snp=vcf_row.ALT, filename=os.path.basename(vcffile),
-                     samples=[s.sample for s in vcf_row.samples],
-                     snp_status=vcf_row.is_snp, indel_status=vcf_row.is_indel) for vcf_row in vcf_rows]
+def get_vcf_rows(vcffile):
+    with pbopen(click.format_filename(vcffile)) as f:
+        vcf_rows = vcf.Reader(f)
+        vcf_dict_list = [
+            dict(pos=int(vcf_row.POS), ref=vcf_row.REF, snp=vcf_row.ALT, filename=os.path.basename(vcffile),
+                    samples=[s.sample for s in vcf_row.samples],
+                    snp_status=vcf_row.is_snp, indel_status=vcf_row.is_indel) for vcf_row in vcf_rows]
     return vcf_dict_list
 
 
@@ -102,26 +119,31 @@ def f_results(result, samples):
                     f_dict['{0}_pos_picked_{1}'.format(record['filename'], i)] = "POS_{}_{}".format(record['pos'],
                                                                                                     str(record['snp_status']).upper())
                 except IndexError:
-                    # TODO: nothing was found - might need revision
+                    # @TODO: nothing was found - might need revision
                     record = r['vcf'][0]
                     f_dict['{0}_pos_picked_{1}'.format(record['filename'], i)] = 'N/A'
             else:
-                # TODO: nothing was found - might need revision
+                # @TODO: nothing was found - might need revision
                 f_dict['{0}_pos_picked_{1}'.format(get_vcf_file_name(result), i)] = 'N/A'
         result_list.append(f_dict)
     return result_list
 
 
-def create_output_files(results):
+def create_output_files(results, out_dir, ref_samples):
     outputfiles = [k for k, v in results[0].items() if k not in ['pos']]
     for o in outputfiles:
-        current_file = open("output/{}.txt".format(o), 'w+')
+        if o in ref_samples:
+            output_filename = "{}/{}.txt".format(out_dir,o)
+        else:
+            output_filename = "{}/vcf/{}.txt".format(out_dir,o)
+        current_file = open(output_filename, 'w+')
         for r in results:
             if str(r[o]) != "N/A":
                 current_file.write("{}\n".format(str(r[o])))
             else:
                 current_file.write("")
         current_file.close()
+    return outputfiles
 
 
 def get_vcf_file_name(result):
